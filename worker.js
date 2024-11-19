00c7c7fdc2845b62f5ffe8aa977ff9fad4096ebf
zrhe2016 <zrhe2016@gmail.com>
2024-11-19 14:54:35 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,5 @@
 import JSZip from 'jszip';
 
-
 export default {
   async fetch(request, env, ctx) {
     try {
@@ -48,7 +47,7 @@ export default {
         
         for (const book of books.results) {
           subSitemap += `  <url>\n`;
-          subSitemap += `    <loc>${baseUrl}/book/${book.id}</loc>\n`;
+          subSitemap += `    <loc>${baseUrl}/${book.id}</loc>\n`;
           subSitemap += `  </url>\n`;
         }
         
@@ -60,12 +59,31 @@ export default {
       }
       
       mainSitemap += '</sitemapindex>';
+
+      // 创建index.html内容
+      let indexHtml = '<!DOCTYPE html>\n<html>\n<head>\n';
+      indexHtml += '<meta charset="UTF-8">\n';
+      indexHtml += '<title>All Book Links</title>\n';
+      indexHtml += '<style>body{font-family:Arial,sans-serif;margin:20px;} a{display:block;margin:5px 0;}</style>\n';
+      indexHtml += '</head>\n<body>\n';
+      
+      // 获取所有书籍ID并添加链接
+      const allBooks = await db.prepare(
+        "SELECT id FROM books"
+      ).all();
+      
+      for (const book of allBooks.results) {
+        indexHtml += `<a href="${baseUrl}/${book.id}">${baseUrl}/${book.id}</a>\n`;
+      }
+      
+      indexHtml += '</body>\n</html>';
       
       // 创建ZIP文件
       const zip = new JSZip();
       
-      // 添加主站点地图到ZIP
+      // 添加主站点地图和index.html到ZIP
       zip.file('sitemap.xml', mainSitemap);
+      zip.file('index.html', indexHtml);
       
       // 添加所有子站点地图到ZIP
       subSitemaps.forEach(sitemap => {
